7a34a80c04052dfdf4f5f70e59b10348652b5df2
ValentinModan <valentinmodan@yahoo.com>
2018-05-05 17:51:45 +0300

    Sending orders

--- KBSJ/src/Test.java
+++ KBSJ/src/Test.java
@@ -12,14 +12,38 @@ public static void main(String[] argv) {
     RacerClient racer = new RacerClient(ip,port);
     try {
          racer.openConnection();
+         
+         
          System.out.println(racer.sendRaw("(racer-read-file " + filename + ")")); 
          System.out.println("-------------");
+
+
+
+         String abox =  racer.getAboxGraph();
+         String cpu = "cpu";
          
-         //racer.addAttributeAssertion('DEFAULT', individual, object, attribute)
-         //System.out.println(racer.sendRaw("(all-atomic-concepts)"));
          
+
+         racer.addRoleAssertion( "DEFAULT", "CPU", "hasSocket",  "pizza");
+         racer.addRoleAxiom("PIZZA","BERE");
+         racer.sendRaw("(DEFINE-PRIMITIVE-ROLE |hasSocketVersion| :RANGE |CPUSocket|) ");
+
+         Object a = null, b = null, c = null;
+        String s = racer.allRoles();
+
+
+        System.out.println("EMPTY SPACE");
+        System.out.println(s);
+        System.out.println("EMPTY SPACE");
+
+        //racer.addAttributeAssertion('DEFAULT', individual, object, attribute)
+
          System.out.println(racer.allAtomicConcepts());
-                  
+
+        System.out.println("EMPTY_SPACE");
+        System.out.println(racer.allAtomicConcepts());
+
+
          racer.closeConnection();
     }
     catch (Exception e) {
